4d41c45c01526c23fd14eabf315cfcc5e7db2783
Simon Geoghegan <simon.geoghegan@workhouseadvertising.com.au>
2018-08-29 14:39:55 +0800

    Moved import error output outside of the try/catch as the log trace in the exception handling doesn't always report on all of the errors

--- Console/Command/ImportCommand.php
+++ Console/Command/ImportCommand.php
@@ -74,10 +74,6 @@ class ImportCommand extends Command
                 $output->writeln($import->getFormattedLogTrace());
             } else {
                 $output->writeln('<error>Import failed.</error>');
-                $errors = $import->getErrors();
-                foreach ($errors as $error) {
-                    $output->writeln('<error>' . $error->getErrorMessage() . ' - ' .$error->getErrorDescription() . '</error>');
-                }
             }
 
         } catch (FileNotFoundException $e) {
@@ -87,6 +83,13 @@ class ImportCommand extends Command
             $output->writeln('<error>Invalid source.</error>');
             $output->writeln("Log trace:");
             $output->writeln($import->getFormattedLogTrace());
+        }
+        $errors = $import->getErrors();
+        if ($errors) {
+            $output->writeln('<error>The following errors were encountered during the import:</error>');
+            foreach ($errors as $error) {
+                $output->writeln('<error>' . $error->getErrorMessage() . ' - ' .$error->getErrorDescription() . '</error>');
+            }
         }
     }
 
